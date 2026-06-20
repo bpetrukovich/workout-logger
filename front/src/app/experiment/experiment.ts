3fcaeec2df39c9438c1eb8442fbad195a4bf2377
bpetrukovich <petrukovichbogdan@gmail.com>
2026-06-20 16:02:53 +0300

    create desing

--- front/src/app/experiment/experiment.ts
+++ front/src/app/experiment/experiment.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-experiment',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatButtonModule,
+  ],
   templateUrl: './experiment.html',
   styleUrl: './experiment.scss',
 })
